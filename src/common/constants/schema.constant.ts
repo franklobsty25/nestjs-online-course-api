9245d3badc52a994e21e9c8e06c7f858761a9fc6
Frank Adu Opoku Kodie <frankkodie@yahoo.com>
2023-01-31 10:07:06 +0000

    conflict on schema fixed

--- src/common/constants/schema.constant.ts
+++ src/common/constants/schema.constant.ts
@@ -3,8 +3,5 @@ export const ROLE = 'role';
 export const COURSE = 'course';
 export const COMMENT = 'comment';
 export const MESSAGE = 'message';
-<<<<<<< HEAD
 export const EMPLOYEE = 'employee';
-=======
 export const ORGANIZATION = 'organization';
->>>>>>> a92c7227e4acbde8f2de85b44048f0458b96c779
